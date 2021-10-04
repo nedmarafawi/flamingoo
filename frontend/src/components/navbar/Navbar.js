9b85695fdc7b8718fad107485a240b03d3f5009b
Ned Marafawi <70671308+nedmarafawi@users.noreply.github.com>
2021-10-03 20:50:45 -0400

    Update Navbar and avatar style

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { keyframes } from 'styled-components';
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import { Context } from '../../context/Context';
@@ -20,26 +21,26 @@ export default function Navbar() {
           The Traveler
         </Link>
       </NavbarLeft>
-      <NavbarCenter>
-        <Lists>
-          <ListItem>
-            {/* <Link to="/" className="link">
-              Home
-            </Link> */}
-          </ListItem>
-
-          <ListItem>
-            <Link to="/posting" className="link">
-              Post
-            </Link>
-          </ListItem>
-        </Lists>
-      </NavbarCenter>
+      {/* <NavbarCenter>
+        <Lists></Lists>
+      </NavbarCenter> */}
       {/* <ListItem onClick={handleLogout}>{user && 'LOGOUT'}</ListItem> */}
-      <NavbarRight>
+      <NavbarRight className="dot">
+        <ListItem>
+          <Link to="/" className="link">
+            Home
+          </Link>
+        </ListItem>
+        <PostItem>
+          <Link to="/posting" className="link">
+            Post
+          </Link>
+        </PostItem>
         {user ? (
           <Link to="/settings">
-            <Image src={publicFolder + user.profileAvatar} alt="" />
+            <Avatar>
+              <Image src={publicFolder + user.profileAvatar} alt="" />
+            </Avatar>
           </Link>
         ) : (
           <Lists>
@@ -56,49 +57,85 @@ export default function Navbar() {
           </Lists>
         )}
         {/* <i className="navbarSearchIcon fas fa-search"></i> */}
+        <ListItem onClick={handleLogout}>
+          {user && <Logout>Logout</Logout>}
+        </ListItem>
       </NavbarRight>
-      <NavbarLogout onClick={handleLogout}>
+      {/* <NavbarLogout onClick={handleLogout}>
         {user && <Logout>Logout</Logout>}
-      </NavbarLogout>
+      </NavbarLogout> */}
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
+  margin-top: 5px;
   width: 100%;
-  height: 50px;
+  height: 60px;
   background-color: white;
   position: sticky;
   top: 0;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   font-family: 'Josefin Sans', sans-serif;
   z-index: 999;
+  box-shadow: 0 1rem 8rem 0 rgba(0, 0, 0, 0.2);
 `;
+// transform: translate(-50%, -50%);
+// flex: 1;
 
 const NavbarLeft = styled.div`
-  flex: 3;
+  margin-left: 150px;
+  margin-bottom: 10px;
+  margin-top: 10px;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
+
+  &:hover {
+    color: #0083a3;
+  }
 `;
 
+// flex: 1;
 const NavbarRight = styled.div`
-  flex: 3;
   display: flex;
   align-items: center;
-  justify-content: center;
-`;
-
-const NavbarLogout = styled.div`
-  font-size: 18px;
-  font-weight: 300;
-  cursor: pointer;
-`;
-
-const Logout = styled.div`
+  justify-content: flex-end;
   margin-right: 50px;
-  margin-left: -190px;
+  margin-bottom: 10px;
+  margin-top: 10px;
+
+  transition: 0.5s;
+
+  &:after {
+    position: absolute;
+    content: '';
+    top: 100%;
+    left: 0;
+    width: 100%;
+    height: 1px;
+    background: #d5d7d8;
+    transform: scaleX(5);
+    transform-origin: right;
+    transition: transform 0.5s;
+  }
+
+  &:hover {
+    color: #95a5a6;
+  }
+`;
+
+// const NavbarLogout = styled.div`
+//   font-size: 18px;
+//   font-weight: 300;
+//   cursor: pointer;
+// `;
+
+// margin-right: 50px;
+const Logout = styled.div`
+  margin-left: 30px;
 `;
 
 // const Icon = styled.div`
@@ -108,17 +145,44 @@ const Logout = styled.div`
 // cursor: pointer;
 // `;
 
+// 0% { border-color: hsl(0, 100%, 50%); }
+//   25% { border-color: hsl(90, 100%, 50%); }
+//   50% { border-color: hsl(180, 100%, 50%); }
+//   75% { border-color: hsl(270, 100%, 50%); }
+//   100% { border-color: hsl(360, 100%, 50%); }
+
+const AvatarAnimated = keyframes`
+0% {
+  -webkit-transform: translateY(-8px);
+  transform: translateY(-8px);
+}
+50% {
+  -webkit-transform: translateY(-4px);
+  transform: translateY(-4px);
+}
+100% {
+  -webkit-transform: translateY(-8px);
+  transform: translateY(-8px);
+}
+`;
+
+const Avatar = styled.div`
+  animation: ${AvatarAnimated} 5s infinite;
+`;
+
 const Image = styled.img`
   width: 40px;
   height: 40px;
   border-radius: 50%;
   object-fit: cover;
   cursor: pointer;
+  margin-top: 10px;
+  margin-left: 10px;
 `;
 
-const NavbarCenter = styled.div`
-  flex: 6;
-`;
+// const NavbarCenter = styled.div`
+//   flex: 6;
+// `;
 
 const Lists = styled.ul`
   display: flex;
@@ -134,4 +198,22 @@ const ListItem = styled.li`
   font-weight: 300;
   cursor: pointer;
   list-style: none;
+
+  &:hover {
+    color: #0083a3;
+  }
+`;
+
+// display: flex;
+// justify-content: flex-end;
+const PostItem = styled.li`
+  margin-right: 20px;
+  font-size: 18px;
+  font-weight: 300;
+  cursor: pointer;
+  list-style: none;
+
+  &:hover {
+    color: #0083a3;
+  }
 `;
